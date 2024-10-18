faa71375f6a469f58e5d52890ef666398477b59e
Edgardo Ambrosi <edgardoambrosi@alia-space.com>
2024-10-18 08:48:19 +0200

    UPDATE

--- filter.js
+++ filter.js
@@ -1,5 +1,20 @@
 const fs = require('fs');
 const path = require('path');
+const http=require('http')
+const axios = require('axios');
+
+// Configure the HTTP agent to reuse the socket
+const agent = new http.Agent({
+  keepAlive: true, // Keep the connection alive for reuse
+  maxSockets: 1,   // Limit to one socket if you want to reuse the same socket
+  maxFreeSockets: 1,
+});
+
+// Axios instance with the custom HTTP agent
+const axiosInstance = axios.create({
+  httpAgent: agent
+});
+
 
 // Funzione per caricare il file JSON
 function loadJSONFile(filePath) {
@@ -43,6 +58,11 @@ function removeKeyIfCondition(jsonData, keysToRemove, conditionKey, conditionVal
 //removeKeyIfCondition(jsonData, keysToRemove, 'href', '');
 //writeJSONFile(jsonFilePath, jsonData);
 
+
+function delay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 const folderPath = './sentinel-2/2024/'
 function processAllFilesInFolderRecursively() {
   fs.readdir(folderPath, (err, files) => {
@@ -52,31 +72,43 @@ function processAllFilesInFolderRecursively() {
     }
     files.forEach((folder) => {
       let _jsonFilePath = folderPath + folder
+      console.log(_jsonFilePath)
       fs.readdir(_jsonFilePath, (err, files) => {
         if (err) {
           console.error('Errore durante la lettura della cartella:', err);
           return;
         }
-        files.forEach((file) => {
+        files.forEach(async (file, index) => {
           jsonFilePath = _jsonFilePath + '/' + file;
-          let jsonData = loadJSONFile(jsonFilePath);
-          if (jsonData) {
-            /*let keysToRemove = ['B02', 'B03', 'B04', 'B08', 'B11', 'B12', 'B8A', 'VV', 'VH'];
-            jsonData['virtual:assets']['RGB']['href']=[]
-            removeKeyIfCondition(jsonData, keysToRemove, 'href', '');
-            writeJSONFile(jsonFilePath, jsonData);*/
+          //let jsonData = loadJSONFile(jsonFilePath);
+          //if (jsonData) {
+          /*let keysToRemove = ['B02', 'B03', 'B04', 'B08', 'B11', 'B12', 'B8A', 'VV', 'VH'];
+          jsonData['virtual:assets']['RGB']['href']=[]
+          removeKeyIfCondition(jsonData, keysToRemove, 'href', '');
+          writeJSONFile(jsonFilePath, jsonData);*/
 
-            if (jsonData['id'].split('_')[5]) {
-              let STARTDATE = jsonData['id'].split('_')[5].substr(0, 4) + '-' + jsonData['id'].split('_')[5].substr(4, 2) + '-' + jsonData['id'].split('_')[5].substr(6, 2) + "T00:00:00+00:00"
-              let DATETIME = jsonData['id'].split('_')[5].substr(0, 4) + '-' + jsonData['id'].split('_')[5].substr(4, 2) + '-' + jsonData['id'].split('_')[5].substr(6, 2) + "T00:00:00Z"
-              let ENDDATE = jsonData['id'].split('_')[6].substr(0, 4) + '-' + jsonData['id'].split('_')[6].substr(4, 2) + '-' + jsonData['id'].split('_')[6].substr(6, 2) + "T00:00:00+00:00"
+          /*if (jsonData['id'].split('_')[5]) {
+            let STARTDATE = jsonData['id'].split('_')[5].substr(0, 4) + '-' + jsonData['id'].split('_')[5].substr(4, 2) + '-' + jsonData['id'].split('_')[5].substr(6, 2) + "T00:00:00+00:00"
+            let DATETIME = jsonData['id'].split('_')[5].substr(0, 4) + '-' + jsonData['id'].split('_')[5].substr(4, 2) + '-' + jsonData['id'].split('_')[5].substr(6, 2) + "T00:00:00Z"
+            let ENDDATE = jsonData['id'].split('_')[6].substr(0, 4) + '-' + jsonData['id'].split('_')[6].substr(4, 2) + '-' + jsonData['id'].split('_')[6].substr(6, 2) + "T00:00:00+00:00"
 
-              jsonData['properties']['datetime'] = DATETIME
-              jsonData['properties']['start_datetime'] = STARTDATE
-              jsonData['properties']['end_datetime'] = ENDDATE
-              writeJSONFile(jsonFilePath, jsonData);
-            }
-          }
+            jsonData['properties']['datetime'] = DATETIME
+            jsonData['properties']['start_datetime'] = STARTDATE
+            jsonData['properties']['end_datetime'] = ENDDATE
+            writeJSONFile(jsonFilePath, jsonData);
+          }*/
+          //}
+
+
+        let url='http://51.91.136.207:8000/stac/info?url=https://nupsi.alia-space.com:3943/stacserver/catalogs/sentinel-2/2024/' + folder + '/' + file
+        try {
+          const response = await axiosInstance.get(url);
+          console.log('OK: ', url );
+        } catch (error) {
+          console.error('KO: ', url);
+        }
+        //await delay(5000);   
+
         });
       });
     });
