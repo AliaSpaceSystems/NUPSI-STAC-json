00ea28988a19df00473a5f0222fe4b0e68b09385
Edgardo Ambrosi <edgardoambrosi@alia-space.com>
2024-10-18 13:05:41 +0200

    FILTRO PER CONTROLLARE I FILE STAC

--- filter.js
+++ filter.js
@@ -1,7 +1,11 @@
 const fs = require('fs');
 const path = require('path');
-const http=require('http')
+const http = require('http')
 const axios = require('axios');
+
+const folderPath = './sentinel-2/2024/'
+var numeroDiFileTotale=0
+let numeroDiFileProcessati = 0;
 
 // Configure the HTTP agent to reuse the socket
 const agent = new http.Agent({
@@ -14,6 +18,20 @@ const agent = new http.Agent({
 const axiosInstance = axios.create({
   httpAgent: agent
 });
+
+
+function delay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function appendToFile(file, linee) {
+  try {
+    fs.appendFileSync(file, linee.join('\n') + '\n');
+    //console.log(`Linee appese con successo a ${file}`);
+  } catch (errore) {
+    //console.error(`Errore: ${errore}`);
+  }
+}
 
 
 // Funzione per caricare il file JSON
@@ -35,6 +53,22 @@ function writeJSONFile(filePath, data) {
   fs.writeFileSync(fullPath, JSON.stringify(data, null, 2), 'utf-8');
 }
 
+
+function contaFileInCartella() {
+  try {
+    const files = fs.readdirSync(folderPath, { recursive: true });
+    const soloFile = files.filter((file) => {
+      const filePath = path.join(folderPath, file);
+      return fs.statSync(filePath).isFile();
+    });
+    return soloFile.length;
+  } catch (err) {
+    console.error('Errore nella lettura della cartella:', err);
+    return 0;
+  }
+}
+
+
 // Funzione per rimuovere una chiave se soddisfa una condizione
 function removeKeyIfCondition(jsonData, keysToRemove, conditionKey, conditionValue) {
   keysToRemove.forEach((targetKey) => {
@@ -51,20 +85,11 @@ function removeKeyIfCondition(jsonData, keysToRemove, conditionKey, conditionVal
 
 }
 
-
-//const jsonFilePath = './sentinel-2/2024/06/stac_EL.json';
-//let jsonData = loadJSONFile(jsonFilePath);
-//const keysToRemove = ['B02', 'B03', 'B04', 'B08', 'B11', 'B12', 'B8A', 'VV', 'VH'];
-//removeKeyIfCondition(jsonData, keysToRemove, 'href', '');
-//writeJSONFile(jsonFilePath, jsonData);
-
-
-function delay(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-const folderPath = './sentinel-2/2024/'
 function processAllFilesInFolderRecursively() {
+
+  numeroDiFileTotale = contaFileInCartella();
+  console.log('numeroDiFileTotale', numeroDiFileTotale)
+
   fs.readdir(folderPath, (err, files) => {
     if (err) {
       console.error('Errore durante la lettura della cartella:', err);
@@ -72,7 +97,6 @@ function processAllFilesInFolderRecursively() {
     }
     files.forEach((folder) => {
       let _jsonFilePath = folderPath + folder
-      console.log(_jsonFilePath)
       fs.readdir(_jsonFilePath, (err, files) => {
         if (err) {
           console.error('Errore durante la lettura della cartella:', err);
@@ -80,39 +104,52 @@ function processAllFilesInFolderRecursively() {
         }
         files.forEach(async (file, index) => {
           jsonFilePath = _jsonFilePath + '/' + file;
-          //let jsonData = loadJSONFile(jsonFilePath);
-          //if (jsonData) {
-          /*let keysToRemove = ['B02', 'B03', 'B04', 'B08', 'B11', 'B12', 'B8A', 'VV', 'VH'];
-          jsonData['virtual:assets']['RGB']['href']=[]
-          removeKeyIfCondition(jsonData, keysToRemove, 'href', '');
-          writeJSONFile(jsonFilePath, jsonData);*/
 
-          /*if (jsonData['id'].split('_')[5]) {
-            let STARTDATE = jsonData['id'].split('_')[5].substr(0, 4) + '-' + jsonData['id'].split('_')[5].substr(4, 2) + '-' + jsonData['id'].split('_')[5].substr(6, 2) + "T00:00:00+00:00"
-            let DATETIME = jsonData['id'].split('_')[5].substr(0, 4) + '-' + jsonData['id'].split('_')[5].substr(4, 2) + '-' + jsonData['id'].split('_')[5].substr(6, 2) + "T00:00:00Z"
-            let ENDDATE = jsonData['id'].split('_')[6].substr(0, 4) + '-' + jsonData['id'].split('_')[6].substr(4, 2) + '-' + jsonData['id'].split('_')[6].substr(6, 2) + "T00:00:00+00:00"
-
-            jsonData['properties']['datetime'] = DATETIME
-            jsonData['properties']['start_datetime'] = STARTDATE
-            jsonData['properties']['end_datetime'] = ENDDATE
-            writeJSONFile(jsonFilePath, jsonData);
-          }*/
-          //}
-
-
-        let url='http://51.91.136.207:8000/stac/info?url=https://nupsi.alia-space.com:3943/stacserver/catalogs/sentinel-2/2024/' + folder + '/' + file
-        try {
-          const response = await axiosInstance.get(url);
-          console.log('OK: ', url );
-        } catch (error) {
-          console.error('KO: ', url);
-        }
-        //await delay(5000);   
-
+          //changeStacFileCollection()
+          checkStacFileOnTitiler(jsonFilePath)
         });
       });
     });
   });
+}
+
+function changeStacFileCollection() {
+  let jsonData = loadJSONFile(jsonFilePath);
+  if (jsonData) {
+
+    let keysToRemove = ['B02', 'B03', 'B04', 'B08', 'B11', 'B12', 'B8A', 'VV', 'VH'];
+    jsonData['virtual:assets']['RGB']['href'] = []
+    removeKeyIfCondition(jsonData, keysToRemove, 'href', '');
+    writeJSONFile(jsonFilePath, jsonData);
+
+    if (jsonData['id'].split('_')[5]) {
+      let STARTDATE = jsonData['id'].split('_')[5].substr(0, 4) + '-' + jsonData['id'].split('_')[5].substr(4, 2) + '-' + jsonData['id'].split('_')[5].substr(6, 2) + "T00:00:00+00:00"
+      let DATETIME = jsonData['id'].split('_')[5].substr(0, 4) + '-' + jsonData['id'].split('_')[5].substr(4, 2) + '-' + jsonData['id'].split('_')[5].substr(6, 2) + "T00:00:00Z"
+      let ENDDATE = jsonData['id'].split('_')[6].substr(0, 4) + '-' + jsonData['id'].split('_')[6].substr(4, 2) + '-' + jsonData['id'].split('_')[6].substr(6, 2) + "T00:00:00+00:00"
+
+      jsonData['properties']['datetime'] = DATETIME
+      jsonData['properties']['start_datetime'] = STARTDATE
+      jsonData['properties']['end_datetime'] = ENDDATE
+      writeJSONFile(jsonFilePath, jsonData);
+    }
+
+  }
+}
+
+async function checkStacFileOnTitiler(jsonFilePath) {
+  let remotePath = jsonFilePath.replace('\.', '')
+  let url = 'http://51.91.136.207:8000/stac/viewer?url=https://nupsi.alia-space.com:3943/stacserver/catalogs' + remotePath
+  try {
+    const response = await axiosInstance.get(url);
+    appendToFile('reportStacFile.txt', ['OK: ' + url]);
+    //console.log('OK: ', url );
+    console.log(numeroDiFileTotale, numeroDiFileProcessati++)
+  } catch (error) {
+    appendToFile('reportStacFile.txt', ['KO: ' + url]);
+    //console.error('KO: ', url);
+    console.log(numeroDiFileTotale, numeroDiFileProcessati++)
+  }
+  await delay(1000);
 }
 
 processAllFilesInFolderRecursively()
